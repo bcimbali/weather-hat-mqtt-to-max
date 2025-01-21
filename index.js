cbb7a21b3516c3c154bd6cac79360298e5cc0d6f
Brett Cimbalik <bcimbali@gmail.com>
2025-01-20 19:14:30 -0500

    feat(data): parse ints from end of float nums

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
-const maxApi = require('max-api');
-const mqtt = require('mqtt');
+const maxApi = require("max-api");
+const mqtt = require("mqtt");
 
 let client;
 
-maxApi.addHandler('connect', () => {
-  console.log('Top of connect addHandler');
+maxApi.addHandler("connect", () => {
+  console.log("Top of connect addHandler");
   // Works fine & we can hardcode it for now:
-  client = mqtt.connect('mqtt://raspberrypi.local:1883');
+  client = mqtt.connect("mqtt://raspberrypi.local:1883");
 
-  client.on('connect', () => {
-    console.log('Top of client.on connect');
-    maxApi.outlet('connected');
-  })
+  client.on("connect", () => {
+    console.log("Top of client.on connect");
+    maxApi.outlet("connected");
+  });
 });
 
-maxApi.addHandler('subscribe', () => {
-  console.log('Top of subscribe handler');
-  client.subscribe('weather');
+maxApi.addHandler("subscribe", () => {
+  console.log("Top of subscribe handler");
+  client.subscribe("weather");
 
-  client.on('message', (topic, message) => {
+  client.on("message", (topic, message) => {
     // Parse the MQTT message:
     const parsedMessage = JSON.parse(message?.toString());
-    console.log('In index.js, this is parsedMessage: ', parsedMessage);
+
+    // Temperature:
+    const tempToString = parsedMessage?.temperature?.toString();
+    const lastIdxOfTemp = parseInt(tempToString?.slice(-1));
+    const secondToLastIdxofTemp = parseInt(
+      tempToString?.[tempToString?.length - 2]
+    );
+
+    // Pressure:
+    const pressureToString = parsedMessage?.pressure?.toString();
+    const lastIdxOfPressue = parseInt(pressureToString?.slice(-1));
+    const secondToLastIdxofPressure = parseInt(
+      pressureToString?.[pressureToString?.length - 2]
+    );
+
+    // Humidity:
+    const humidityToString = parsedMessage?.humidity?.toString();
+    const lastIdxOfHumidity = parseInt(humidityToString?.slice(-1));
+    const secondToLastIdxofHumidity = parseInt(
+      humidityToString?.[humidityToString?.length - 2]
+    );
+    const thirdToLastIdxofHumidity = parseInt(
+      humidityToString?.[humidityToString?.length - 3]
+    );
+
+    // console.log('In index.js, this is parsedMessage: ', parsedMessage);
 
     // Grab individual weather hat values:
     const temperature = parsedMessage?.temperature;
@@ -32,8 +57,23 @@ maxApi.addHandler('subscribe', () => {
     const light = parsedMessage?.light;
     const windspeed = parsedMessage?.wind_speed;
     const windDirection = parsedMessage?.wind_direction;
-    
+
     // Send out to Max as a list of floating point numbers:
-    maxApi.outlet(temperature, pressure, humidity, relativeHumidity, dewpoint, light, windspeed, windDirection );
-  })
-})
+    maxApi.outlet(
+      temperature,
+      pressure,
+      humidity,
+      relativeHumidity,
+      dewpoint,
+      light,
+      windspeed,
+      lastIdxOfTemp,
+      secondToLastIdxofTemp,
+      lastIdxOfPressue,
+      secondToLastIdxofPressure,
+      lastIdxOfHumidity,
+      secondToLastIdxofHumidity,
+      thirdToLastIdxofHumidity
+    );
+  });
+});
